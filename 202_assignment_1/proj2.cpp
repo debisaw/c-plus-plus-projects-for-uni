a852a8daa8a50e0ac7ba0703856db52dbcd16574
debisaw <98407664+debisaw@users.noreply.github.com>
2022-02-25 19:32:17 -0500

    Add files via upload

---
+++ 202_assignment_1/proj2.cpp
@@ -0,0 +1,21 @@
+//Title: Project 2.cpp
+//name:Dan Ebisawa
+//date:10/18/18
+//section: 8
+//desctiption: play the fortnite dance game
+
+#include "Hero.h"
+#include "Game.h"
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main () {
+  Game newGame;
+  newGame.StartGame();
+  return 0;
+}
